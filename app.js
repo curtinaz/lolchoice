cc2a9c8b044cb6f7209d0da62af0a21241c87dff
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-16 20:04:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ app.get('/', function (req, res) {
     res.send('Rotas funcionando')
   })
 
+app.post('/api', function (req, res) {
+    res.send('data.json')
+})
+
 app.listen(3000)
 console.log("server is running")
