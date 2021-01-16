6d95ad60bc45039779dfd43b9df77dc095ecaf92
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-16 19:58:57 -0300

    Update rotas.js

--- rotas.js
+++ rotas.js
@@ -1,5 +1,5 @@
 const app = require('./app.js')
-const rotas = require('./app.js', 'routes');
+const rotas = require('app.routes');
 
 rotas.get('/', function (req, res) {
     res.send('Rotas funcionando')
