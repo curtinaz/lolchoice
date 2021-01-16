d5e129c41c98175b813f687cc87c6d7d1d4e94b3
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-16 20:00:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const routes = express.Router();
 
+// ROTAS
+
+app.get('/', function (req, res) {
+    res.send('Rotas funcionando')
+  })
+
 app.listen(3000)
 console.log("server is running")
 
