704ad3fa306be1dbe8dbca6c10c9973bdf77ab02
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-16 19:57:18 -0300

    rotas is now workin

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const express = require('express');
 const app = express();
 const routes = express.Router();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
-
 app.listen(3000)
 console.log("server is running")
 
